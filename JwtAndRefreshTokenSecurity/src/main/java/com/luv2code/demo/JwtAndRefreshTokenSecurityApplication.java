6abd96da73609746b2fbe88342c3218ce375234b
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2024-05-17 19:38:13 +0300

    Jwt And Refresh Token Spring Security.

---
+++ JwtAndRefreshTokenSecurity/src/main/java/com/luv2code/demo/JwtAndRefreshTokenSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAndRefreshTokenSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAndRefreshTokenSecurityApplication.class, args);
+	}
+
+}
---
+++ JwtAndRefreshTokenSecurity/src/main/java/com/luv2code/demo/service/RefreshTokenService.java
@@ -0,0 +1,13 @@
+package com.luv2code.demo.service;
+
+import com.luv2code.demo.entity.RefreshToken;
+
+public interface RefreshTokenService {
+
+	RefreshToken createRefreshToken(String username);
+
+	RefreshToken findByToken(String token);
+
+	RefreshToken verifyExpiration(RefreshToken token);
+
+}
